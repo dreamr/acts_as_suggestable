811064dd43aa70bf8587b93d233ef41d9c735dda
James OKelly <james@rubyloves.me>
2011-04-14 01:55:16 -0700

    ..

--- lib/generators/acts_as_suggestable/acts_as_suggestable_generator.rb
+++
@@ -1,7 +0,0 @@
-class ActsAsSuggestableGenerator < Rails::Generator::Base 
-  def manifest 
-    record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "create_suggestables"
-    end
-  end
-end
