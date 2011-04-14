37069f773afffde2e1ca596709c344382e1b4445
James OKelly <james@rubyloves.me>
2011-04-14 15:31:21 -0700

    namespaced sugestable model

--- lib/acts_as_suggestable/suggestable.rb
+++ lib/acts_as_suggestable/suggestable.rb
@@ -1,13 +1,13 @@
 require 'active_record'
 
-class Suggestable < ActiveRecord::Base
+class ActsAsSuggestable::Suggestable < ActiveRecord::Base
   belongs_to :user
   belongs_to :suggested_by, :polymorphic => true
   belongs_to :suggestion,   :polymorphic => true
-  
+
   scope :by_user_id,  lambda {|user_id| 
                         where('suggestables.user_id = ?', user_id)}
-        
+
   scope :by_type,     lambda {|type| 
                         where('suggestables.suggestion_type = ?', type.to_s)}
 end
