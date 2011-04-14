e094ba68a315a9414e933ea06c05305d4796edf0
James OKelly <james@rubyloves.me>
2011-04-14 01:44:16 -0700

    ..

--- generators/acts_as_suggestable_migration/acts_as_suggestable_migration_generator.rb
+++ generators/acts_as_suggestable_migration/acts_as_suggestable_migration_generator.rb
@@ -1,4 +1,4 @@
-class ActsAsTaggableOnMigrationGenerator < Rails::Generator::Base 
+class ActsAsSuggestionMigrationGenerator < Rails::Generator::Base 
   def manifest 
     record do |m| 
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "acts_as_suggestable_migration"
